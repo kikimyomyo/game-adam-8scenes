adebe28cc95b0716e6cd014b79031579b95ad111
kikimyomyo <request1004@gmail.com>
2026-05-09 13:31:01 +0900

    deploy: adam-8scenes

---
+++ config.js
@@ -0,0 +1,6 @@
+window.GAME_CONFIG = {
+  game: "adam-8scenes",
+  title: "아담 8장면",
+  sheet_tab: "adam-8scenes",
+  endpoint: "https://script.google.com/macros/s/AKfycbx3a96h-vRywYTCxOJxdn199uaFAELyfzy7LRIGeInFuG-ZhxsBh_AKso--Yw8PbYPH/exec",
+};
